83cf92e985a0ca1983030ac4630406d8ef5be9b6
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-20 20:49:47 +0700

    modify component system location nb. module core path condition

--- views/o/address/admin_manage.php
+++ views/o/address/admin_manage.php
@@ -94,7 +94,7 @@
 				'template' => '{view}|{update}|{delete}',
 			));
 
-			$this->widget('application.components.system.OGridView', array(
+			$this->widget('application.libraries.core.components.system.OGridView', array(
 				'id'=>'dbs-student-address-grid',
 				'dataProvider'=>$model->search(),
 				'filter'=>$model,
