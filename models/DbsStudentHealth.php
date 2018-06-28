a225161be06c852f942975247a1ed6c760f3a3cc
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-06-28 14:11:08 +0700

    add count column condition in getInfo function

--- models/DbsStudentHealth.php
+++ models/DbsStudentHealth.php
@@ -319,13 +319,16 @@ class DbsStudentHealth extends CActiveRecord
 	{
 		if($column != null) {
 			$model = self::model()->findByPk($id,array(
-				'select' => $column
+				'select' => $column,
 			));
-			return $model->$column;
+ 			if(count(explode(',', $column)) == 1)
+ 				return $model->$column;
+ 			else
+ 				return $model;
 			
 		} else {
 			$model = self::model()->findByPk($id);
-			return $model;			
+			return $model;
 		}
 	}
 
