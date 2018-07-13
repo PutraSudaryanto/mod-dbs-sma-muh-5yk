624dd8952a5cf7db94d5eb0b743813cea411b776
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-13 12:54:06 +0700

    move getGridColumn function to trait

--- controllers/o/HealthController.php
+++ controllers/o/HealthController.php
@@ -179,15 +179,7 @@ class HealthController extends Controller
 			$model->attributes=$_GET['DbsStudentHealth'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'Dbs Student Healths Manage');
 		$this->pageDescription = '';
